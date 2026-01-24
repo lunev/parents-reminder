cf10eae77c2a89c26518c6c30e62417a43169f32
Olexandr L <lunev@aspentechlabs.com>
2026-01-24 12:11:43 +0100

    settings page

--- src/pages/reminder/ReminderAdd.tsx
+++ src/pages/reminder/ReminderAdd.tsx
@@ -1,16 +1,28 @@
 import { Link } from "react-router";
+import { ArrowLeft } from "lucide-react";
 import { ROUTES } from "@/config";
 import { ReminderForm } from "./components/ReminderForm";
+import { Button } from "@/components/ui/button";
 
 export const ReminderAdd = () => {
   return (
-    <>
+    <div className="animate-in slide-in-from-right-50 duration-400">
       <header className="min-h-16.25 bg-card border-b border-border px-4 py-3 flex gap-3 items-center">
-        <Link to={ROUTES.HOME}>Logo</Link>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="w-8 h-8 rounded-full hover:bg-accent"
+          asChild
+        >
+          <Link to={ROUTES.HOME}>
+            <ArrowLeft className="w-5 h-5" />
+          </Link>
+        </Button>
+        <h1 className="text-base font-bold text-foreground capitalize">Add Child</h1>
       </header>
-      <div className="p-5 bg-background">
+      <div className="bg-background">
         <ReminderForm />
       </div>
-    </>
+    </div>
   );
 };
--- src/pages/reminder/components/ReminderForm.tsx
+++ src/pages/reminder/components/ReminderForm.tsx
@@ -41,7 +41,7 @@ export const ReminderForm = () => {
 
   return (
     <form className="flex flex-col gap-7" onSubmit={handleSubmit}>
-      <div className="flex flex-col gap-7 max-h-120 overflow-auto scrollbar-hide">
+      <div className="p-5 flex flex-col gap-7 max-h-120 overflow-auto scrollbar-hide">
         {/* Photo */}
         <div className="pt-2 flex justify-center">
           <PhotoFied
