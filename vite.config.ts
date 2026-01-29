a23aa59c2e35ef5267d5547d2bc38af62c56432c
Olexandr L <lunevdev@gmail.com>
2026-01-29 11:33:51 +0100

    build prev && release features

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/manifest.json",
-          dest: ".",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 
   build: {
     outDir: "build",
